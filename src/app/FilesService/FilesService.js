8c6cf1a33f335fc92851543bf55be49ac589e6f5
amberlynn <amberlynn364@gmail.com>
2024-02-04 15:55:33 +0300

    feat: implement Files service

---
+++ src/app/FilesService/FilesService.js
@@ -0,0 +1,101 @@
+import { access, readdir, rename, stat, unlink, writeFile } from 'fs/promises';
+import path, { basename, join } from 'path';
+import { sortItemsForLS } from '../../utils/sortItemsForLS.js';
+import { createReadStream, createWriteStream } from 'fs';
+import { operationFailed } from '../../utils/operationFailed.js';
+import { printCurrentDirectory } from '../../utils/printCurrentDirectory.js';
+
+export class FilesService {
+  async listDirectory(currentWorkingDirectory) {
+    try {
+      const content = await readdir(currentWorkingDirectory);
+      const items = await Promise.all(content.map(async item => {
+        const fullPath = join(currentWorkingDirectory, item);
+        const type = (await stat(fullPath)).isDirectory() ? 'Directory' : 'File';
+        return { item, type };
+      }));
+
+      sortItemsForLS(items);
+      
+      console.table(items, ['item', 'type']);
+      printCurrentDirectory();
+    } catch {
+      operationFailed();
+    }
+  }
+
+  readFile(filePath) {
+    const fileStream = createReadStream(filePath, 'utf-8');
+
+    fileStream.on('data', chunk => {
+      console.log(chunk);
+      printCurrentDirectory();
+    });
+
+    fileStream.on('error', () => operationFailed());
+  }
+
+  async createFile(currentWorkingDirectory, fileName) {
+    try {
+      const filePath = join(currentWorkingDirectory, fileName);
+      await writeFile(filePath, '', { flag: 'wx' });
+      printCurrentDirectory();
+    } catch {
+      operationFailed();
+    }
+  }
+
+  async renameFile(currentWorkingDirectory, oldFile, newFile) {
+    try {
+      const oldFilePath = join(currentWorkingDirectory, oldFile);
+      const newFilePath = join(path.dirname(oldFilePath), newFile);
+      await rename(oldFilePath, newFilePath);
+      printCurrentDirectory();
+    } catch {
+      operationFailed();
+    }
+  }
+
+  async copyFile(currentWorkingDirectory, filePath, directoryPath) {
+    try {
+      const sourceFilePath = join(currentWorkingDirectory, filePath);
+      const destinationDirectoryPath = join(currentWorkingDirectory, directoryPath);
+
+      await access(sourceFilePath);
+      await access(destinationDirectoryPath);
+      
+      const fileName = basename(sourceFilePath);
+      const sourceStream = createReadStream(sourceFilePath);
+      const destinationPath = join(destinationDirectoryPath, fileName);
+      const destinationStream = createWriteStream(destinationPath);
+
+      sourceStream.pipe(destinationStream);
+
+      await new Promise((resolve, reject) => {
+        sourceStream.on('end', resolve);
+        sourceStream.on('error', reject);
+      })
+      printCurrentDirectory();
+    } catch {
+      operationFailed();
+    }
+  }
+
+  async moveFile(currentWorkingDirectory, filePath, directoryPath) {
+    try {
+      await this.copyFile(currentWorkingDirectory, filePath, directoryPath);
+      await unlink(filePath);
+    } catch {
+    }
+  }
+
+  async removeFile(currentWorkingDirectory, fileName) {
+    try {
+      const filePath = join(currentWorkingDirectory, fileName);
+      await unlink(filePath);
+      printCurrentDirectory();
+    } catch {
+      operationFailed();
+    }
+  }
+}
