fc01e06821aac6fc6b4e65e81e919dc6fd122ad8
amberlynn <amberlynn364@gmail.com>
2024-02-04 16:25:29 +0300

    feat: implement Zip service

---
+++ src/app/ZipService/ZipService.js
@@ -0,0 +1,41 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join, dirname } from 'path'
+import { createBrotliCompress, createBrotliDecompress } from 'zlib'
+import { operationFailed } from '../../utils/operationFailed.js';
+
+export class ZipSerice {
+  async copmressFile(currentWorkingDirectory, oldFile, newFile) {
+    try {
+      const oldFilePath = join(currentWorkingDirectory, oldFile);
+      const newFilePath = join(dirname(oldFilePath), newFile);
+
+      const sourceStream = createReadStream(oldFilePath);
+      const destinationStream = createWriteStream(newFilePath);
+      const brotliCompressor = createBrotliCompress();
+
+      sourceStream.pipe(brotliCompressor).pipe(destinationStream);
+
+      await new Promise((resolve, reject) => {
+        destinationStream.on('finish', resolve);
+        destinationStream.on('error', reject);
+      })
+    } catch {
+      operationFailed();
+    }
+  }
+
+  async decompressFile(currentWorkingDirectory, oldFile, newFile) {
+    try {
+      const oldFilePath = join(currentWorkingDirectory, oldFile);
+      const newFilePath = join(dirname(oldFilePath), newFile);
+
+      const sourceStream = createReadStream(oldFilePath);
+      const destinationStream = createWriteStream(newFilePath);
+      const brotliDecompressor = createBrotliDecompress();
+
+      sourceStream.pipe(brotliDecompressor).pipe(destinationStream);
+    } catch {
+      operationFailed();
+    }
+  }
+}
