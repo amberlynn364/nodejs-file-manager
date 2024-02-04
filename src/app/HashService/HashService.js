ada20beb296cdaff9a60bdb2d7c52eda906d0693
amberlynn <amberlynn364@gmail.com>
2024-02-04 16:02:59 +0300

    feat: implement Hash service

---
+++ src/app/HashService/HashService.js
@@ -0,0 +1,22 @@
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { operationFailed } from '../../utils/operationFailed.js';
+
+export class HashService {
+  async calculateHash(currentWorkingDirectory, fileName) {
+    try {
+      const pathToFile = join(currentWorkingDirectory, fileName);
+      const hash = createHash('sha256');
+      const input = createReadStream(pathToFile);
+
+      return new Promise((resolve, reject) => {
+        input.on('data', (data) => hash.update(data));
+        input.on('end', () => resolve(console.log(hash.digest('hex'))));
+        input.on('error', () => reject());
+      });
+    } catch {
+      operationFailed();
+    }
+  }
+}
